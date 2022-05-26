809480ca3244787c34f0d9084ea026e75dadfc11
turalismylv <turalhi@code.edu.az>
2022-05-26 15:48:27 +0400

    HomeWork26.05

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tapsiriq:
+            // Student classiniz olsun. int[] Grades propertysi olsun. 
+            // Studentden obyekt yaradib grades-i set edende arrayin yanliz cut ededlerinden ibaret yeni bir array set olunsun. 
+            //Yeni eger menStudent st = new Student();         
+            //st.Grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }
+            //yaziramsa st.Grades { 2,4,6,8}-e beraber olsun 
+
+            Student st = new Student();
+            st.Grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            foreach (var item in st.Grades)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        private int[] _grades;
+        public int[] Grades
+
+        {
+            get
+            {
+                return _grades;
+            }
+            set
+            {
+                int[] evenarr = new int[0];
+                int j = 0;
+                
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (value[i] % 2 == 0)
+                    {
+                        Array.Resize(ref evenarr, evenarr.Length + 1);
+                        evenarr[j] = value[i];
+                        j++;
+                    }
+                }
+                if (evenarr.Length>0)
+                {
+                    _grades = evenarr;
+                }
+                else
+                {
+                    _grades = new int[] { 0 };
+                }
+                }
+            }
+        }
+    }
+
+
+
+
+
+
